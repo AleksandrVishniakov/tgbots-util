cb12b63b658a107ee781efcb934a8d9abc8d84bf
AleksandrVishniakov <vishniakov.alexandr@gmail.com>
2025-06-20 18:30:19 +0300

    debug logger

--- logger/context_handler.go
+++ logger/context_handler.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"context"
+	"log"
 	"log/slog"
 
 	"github.com/AleksandrVishniakov/tgbots-util/ctxutil"
@@ -13,7 +14,10 @@ type ContextHandler struct {
 
 func (h *ContextHandler) Handle(ctx context.Context, r slog.Record) error {
     if requestID, ok := ctx.Value(ctxutil.ContextKey_RequestID).(string); ok {
+		log.Println("Got Request ID", requestID)
         r.AddAttrs(slog.String(ctxutil.ContextKey_RequestID.String(), requestID))
-    }
+    } else {
+		log.Println("no request id")
+	}
     return h.Handler.Handle(ctx, r)
 }
